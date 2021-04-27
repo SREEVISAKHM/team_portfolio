49cf2d6b2189c58fc35ba2a5f7157e083f14842b
SREEVISAKHM <sreevisakh0@gmail.com>
2021-04-27 21:22:16 +0530

    team portfolio demo compleated

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -15,85 +15,135 @@ class _HomeState extends State<Home> {
     return Scaffold(
       body: Container(
         color: Colors.white,
-        child: Padding(
-          padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Container(
-                        width: 80.0,
-                        height: 30.0,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(8.0),
-                          color: const Color(0xffffffff),
-                          border: Border.all(
-                              width: 1.0, color: const Color(0xff707070)),
-                        ),
-                        child: Center(
-                          child: Text(
-                            'ABOUT',
-                            style: TextStyle(
-                              fontSize: 15,
-                              fontFamily: 'Roboto',
-                              color: const Color(0xff707070),
-                              fontWeight: FontWeight.bold,
-                            ),
-                            textAlign: TextAlign.left,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Container(
+                      width: 80.0,
+                      height: 30.0,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8.0),
+                        color: const Color(0xffffffff),
+                        border: Border.all(
+                            width: 1.0, color: const Color(0xff707070)),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'ABOUT',
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontFamily: 'Roboto',
+                            color: const Color(0xff707070),
+                            fontWeight: FontWeight.bold,
                           ),
+                          textAlign: TextAlign.left,
                         ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 30,
+                    )
+                  ],
+                ),
+              ),
+              Text(
+                'HI THERE !',
+                style: TextStyle(fontSize: 30, fontFamily: 'Roboto'),
+              ),
+              Text(
+                'meet our team ...',
+                style: TextStyle(fontSize: 40, fontFamily: 'Pacifico'),
+              ),
+              Center(
+                child: Container(
+                  height: 200,
+                  width: 200,
+                  child: Lottie.asset('assets/lottie/coding.json'),
+                ),
+              ),
+              Container(
+                height: 400,
+                width: double.infinity,
+                child: Center(
+                  child: ListView(
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      ProfileCard(
+                        name: 'SREEVISAKH.M',
+                        jobdesignation: 'Flutter developer',
+                        qualification:
+                            'B Tech in Electronics and communication',
+                        experiance: 'Currently working as a freelancer ',
+                        imagename: 'sreevisakh.jpg',
+                      ),
+                      SizedBox(
+                        width: 10,
                       ),
                     ],
                   ),
                 ),
-                Text(
-                  'HI THERE !',
-                  style: TextStyle(fontSize: 30, fontFamily: 'Roboto'),
-                ),
-                Text(
-                  'meet our team ...',
-                  style: TextStyle(fontSize: 40, fontFamily: 'Pacifico'),
-                ),
-                Center(
-                  child: Container(
-                    height: 200,
-                    width: 200,
-                    child: Lottie.asset('assets/lottie/coding.json'),
-                  ),
-                ),
-                Container(
-                  height: 400,
-                  width: double.infinity,
-                  child: Center(
-                    child: ListView(
-                      shrinkWrap: true,
-                      scrollDirection: Axis.horizontal,
-                      children: [
-                        ProfileCard(),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        ProfileCard(),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        ProfileCard(),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        ProfileCard(),
-                      ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                color: Colors.black,
+                height: 200,
+                width: double.infinity,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 30,
                     ),
-                  ),
-                )
-              ],
-            ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: Text(
+                        'Contact us',
+                        style: TextStyle(
+                            fontSize: 30,
+                            color: Colors.white,
+                            fontFamily: 'Roboto'),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: Text(
+                        'Mobile :- +91-9746450223',
+                        style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.white,
+                            fontFamily: 'Roboto'),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: Text(
+                        'Tirur \nMalappuram (d)\npin: 676109',
+                        style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.white,
+                            fontFamily: 'Roboto'),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),
--- lib/screens/widgets/profile_card.dart
+++ lib/screens/widgets/profile_card.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
 
 class ProfileCard extends StatelessWidget {
-  const ProfileCard({Key key}) : super(key: key);
+  final String name;
+  final String jobdesignation;
+  final String qualification;
+  final String experiance;
+  final String imagename;
+
+  ProfileCard(
+      {this.name,
+      this.jobdesignation,
+      this.qualification,
+      this.experiance,
+      this.imagename});
 
   @override
   Widget build(BuildContext context) {
@@ -11,11 +22,135 @@ class ProfileCard extends StatelessWidget {
           padding: const EdgeInsets.only(top: 50),
           child: Container(
             width: 260.0,
-            height: 300.0,
+            height: 330.0,
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(35.0),
               color: const Color(0xffffffff),
               border: Border.all(width: 1.0, color: const Color(0xff707070)),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.only(top: 53, left: 5, right: 5),
+              child: Column(
+                children: [
+                  Text(
+                    name,
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontFamily: 'Source Sans Pro',
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    jobdesignation,
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontFamily: 'Source Sans Pro',
+                    ),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Container(
+                    height: 1,
+                    width: 60,
+                    color: Colors.amber,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      'Qualification',
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontFamily: 'Pacifico',
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    qualification,
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontFamily: 'Source Sans Pro',
+                    ),
+                  ),
+                  SizedBox(
+                    height: 3,
+                  ),
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      'Experiance',
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontFamily: 'Pacifico',
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    experiance,
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontFamily: 'Source Sans Pro',
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 35,
+                        height: 35,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage('assets/images/github.png'),
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Container(
+                        width: 35,
+                        height: 35,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage('assets/images/linkedin.png'),
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Container(
+                        width: 35,
+                        height: 35,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage('assets/images/whatsapp.png'),
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
@@ -26,7 +161,8 @@ class ProfileCard extends StatelessWidget {
           child: Center(
             child: CircleAvatar(
               radius: 50.0,
-              backgroundColor: Colors.amber,
+              backgroundColor: Colors.transparent,
+              backgroundImage: AssetImage('assets/images/$imagename'),
             ),
           ),
         ),
